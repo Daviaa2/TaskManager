43eb7e16f6a0e81acf16ea7df25f597856ca50ba
Daviaa2 <Daviaa2@student.op.ac.nz>
2025-07-29 15:35:25 +1200

    added schema to Task.js in the models folder

---
+++ backend/models/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    completed: {type: Boolean, default: false}
+  }
+);
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String},
+    priority: { type: String, enum: ["Low","Medium", "High"], default: "Medium"},
+    status: { type: String, enum ["Pending", "In Progress","Completed"], default: "Pending" },
+    dueDate: { type: Date, required: true },
+    assignedTo: [ {type: mongoose.Schema.Types.ObjectId, ref: "User"} ],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    attechments: [{type: String}],
+    todoCheckList: [TodoSchema],
+    progress: { type: Number, default: 0 }
+  },
+  
+  { timeseries: true }
+);
+
+module.exports = mongoose.model("Tasks", TaskSchema);
